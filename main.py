91ff85a461e5faade45c254187da947ff127c149
Osmian101 <r093r25@gmail.com>
2020-09-13 13:31:08 -0400

    just cleaned up main

--- main.py
+++ main.py
@@ -14,22 +14,13 @@ def build_players():
     return players
 
 
-def play(players, hole):
-    for player in players:
-        pass
-
-
 def main():
-
     players = build_players()
     course = Course.generate_course()
     score_card = ScoreCard.generate_score_card(players, course)
-    for player in players:
-        pass
-
     print(score_card)
+
     print(course)
-    hole1 = Hole(3, 500)
 
 
 if __name__ == '__main__':
